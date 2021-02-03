ea9308da958d1ad38e17b7b0f596317aaa788475
Intbeam <>
2021-02-03 23:24:29 +0100

    Added license texts. Removed unnecessary definitions. Updated workspace

--- 2021-01-30/build.ps1
+++ 2021-01-30/build.ps1
@@ -15,29 +15,59 @@ param(
     [parameter(Mandatory = $false)][switch]$release = $false
 )
 
+[string]$operatingSystem = "Windows"
 # Watcom install path - assume root
-[string]$watcomInstallPath = "C:\WATCOM"
+[string]$watcomInstallPath = "C:/WATCOM"
+
+# if platform is Unix change install path
+if($PSVersionTable.Platform -eq "Unix")
+{
+    $operatingSystem = "Unix-like"
+    $watcomInstallPath = "/usr/bin/watcom"    
+}
 
 # Retrieve all source code files (c, cpp) from src directory
 $files = (Get-ChildItem -Path "./src" -Filter "*.c*" -Recurse | Where-Object { $_.Extension.ToLowerInvariant() -in (".c", ".cpp")} | Select-Object -ExpandProperty FullName | ForEach-Object { """{0}""" -f $_ }  )
-[string]$fileString = $files | Join-String -Separator " "
 
 # Check environment variables. If Environment contains watcom install path we will assume it has been set correctly
-if($env:Path.contains($watcomInstallPath) -ne $true)
+if($env:PATH.contains($watcomInstallPath) -ne $true)
 {
     # Deconstruct environment path
-    $existingPath = $env:Path -split ";"
+    $existingPath = $env:PATH -split ";"
 
-    # Add watcom paths
-    $existingPath = $existingPath + ("$watcomInstallPath\binnt64", "$watcomInstallPath\binnt")
+
+    if($operatingSystem -eq "Windows")
+    {
+        # Add watcom paths
+        $existingPath += ("$watcomInstallPath/binnt64", "$watcomInstallPath/binnt")
+    }
+    else
+    {
+        $existingPath += ("$watcomInstallPath/binl")
+    }
+
+    [string]$pathSeparator = ";"
+
+    if($operatingSystem -ne "Windows")
+    {
+        $pathSeparator = ":"
+    }
 
     # Set new PATH environment variables
-    $env:Path = ($existingPath | Join-String -Separator ";")
+    $env:PATH = ($existingPath | Join-String -Separator $pathSeparator)
 
     # Set relevant environment variables
     $env:WATCOM = $watcomInstallPath
-    $env:EDPATH = "$watcomInstallPath\EDDAT"
-    $env:INCLUDE = "$watcomInstallPath\H;$watcomInstallPath\H\NT"
+    $env:EDPATH = "$watcomInstallPath/eddat"
+    
+    if($operatingSystem -eq "Windows")
+    {
+        $env:INCLUDE = "$watcomInstallPath/h;$watcomInstallPath/h/nt"
+    }
+    else
+    {
+        $env:INCLUDE = "$watcomInstallPath/h"
+    }
 }
 
 # Create bin and obj directories if they don't exist
@@ -52,10 +82,10 @@ if(-not (Test-Path "./obj"))
 }
 
 # define wcl arguments here
-[string[]]$wclArguments = ("/fo=""./obj/""", "/bc", "/fe=""./bin/main.exe""")
+[string[]]$wclArguments = ("-fo=""./obj/""", "-bc", "-bt=DOS", "-fe=""./bin/main.exe""")
 
 # append files to compile to arguments
 $wclArguments += $files
 
 # Compile and link. Output files to obj directory and put the binary in bin
-& 'wcl.exe' $wclArguments
+& "wcl" $wclArguments
--- 2021-01-30/src/pixelwriter.cpp
+++ 2021-01-30/src/pixelwriter.cpp
@@ -1,3 +1,18 @@
+/* Copyright 2021 Intbeam
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), 
+to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, 
+and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. 
+IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, 
+WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+*/
+
 #include "pixelwriter.hpp"
 
 void VgaPixelWriter::set_next_line_skip_count(int count)
@@ -91,7 +106,7 @@ void VgaPixelWriter::write_pixeldata(int x_origin, int y_origin, const pixel_pac
  void VgaPixelWriter::get_surface_dimensions(surface_dimension &dimension)
  {
      // hardcoded for mode 13
-     
+
      dimension.width = 320;
      dimension.height = 200;
  }
